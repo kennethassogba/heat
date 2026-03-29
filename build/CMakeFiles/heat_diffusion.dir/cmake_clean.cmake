de587a0547082475941f0b7256922b5393c406f7
Kenneth Assogba <kennethassogba@gmail.com>
2026-03-29 18:20:45 +0200

    serial impl

---
+++ build/CMakeFiles/heat_diffusion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heat_diffusion.dir/link.d"
+  "CMakeFiles/heat_diffusion.dir/main.cpp.o"
+  "CMakeFiles/heat_diffusion.dir/main.cpp.o.d"
+  "heat_diffusion"
+  "heat_diffusion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heat_diffusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
